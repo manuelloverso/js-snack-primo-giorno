54342caa2029e23d839d41d69cea1ac924445d2c
manuelloverso <manuelloverso@gmail.com>
2024-02-16 15:37:12 +0100

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+const number = 100; //number
+console.log(number);
+
+let pi = 3.14; //number
+console.log(pi);
+
+// avendo dichiarato la variabile let e non const posso cambiarle il valore
+
+pi = 3.1416; //number
+console.log(pi);
+
+const radius = 8; //number
+const circle = radius * pi * 2; //number
+console.log(circle);
+
+let userName = "Carlo"; //string
+userName = "Marco"; //string
+console.log(userName);
